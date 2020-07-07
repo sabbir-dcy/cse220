da3de39d155c1d1b4ace7282fced556b1b819cf7
sabbir-dcy <sabbirgitcodes@gmail.com>
2020-07-07 20:33:25 +0600

    codes

--- src/Lecture3/resizeCircularArray/main.py
+++ src/Lecture3/resizeCircularArray/main.py
@@ -1,21 +1,20 @@
-import numpy as np
+import array as arr
 
 class Resize():
   def __init__(self, circ):
     self.circ = circ
 
   def expand(self, increaseBy, start):
-    brr = np.zeros(shape=len(circ) + increaseBy, dtype='int')
-    index = start
+    brr = arr.array('i', (len(self.circ) + increaseBy) * [0])
+    newIndex = natIndex = start
     for i in range(len(self.circ)):
-      brr[index] = self.circ[start]
-      start = (start + 1) % len(self.circ)
-      index = (index + 1) % len(brr)
+      brr[newIndex] = self.circ[natIndex]
+      natIndex = (natIndex + 1) % len(self.circ)
+      newIndex = (newIndex + 1) % len(brr)
     return brr
 
 
 if __name__ == "__main__":
-  circ = np.array([3, 4, 5, 1, 2], dtype='int')
-  resize = Resize(circ)
+  resize = Resize(arr.array('i',[3, 4, 5, 1, 2]))
   circ = resize.expand(2, 3)
   print(circ)
