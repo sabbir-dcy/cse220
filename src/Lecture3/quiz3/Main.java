ca5d40a206b96f8edcb3f3661aa25b3f58a27835
sabbir-dcy <sabbirgitcodes@gmail.com>
2020-07-07 19:52:03 +0600

    code

--- src/Lecture3/quiz3/Main.java
+++ src/Lecture3/quiz3/Main.java
@@ -2,9 +2,9 @@ import java.util.Arrays;
 
 public class Main {
   public static void main(String[] args) {
-    int[] arr = {6, 0, 0, 0, 0, 1, 2, 3, 4, 5};
+    int[] arr = {4, 5, 6, 7, 8, 9, 10, 11, 0, 0, 0, 0, 1, 2, 3};
     var modify = new Modify();
-    modify.reverse(arr, 6, 5);
+    modify.reverse(arr, 11, 12);
     System.out.println(Arrays.toString(arr));
   }
 }
@@ -15,8 +15,6 @@ public class Modify {
     int i = 0;
     while (i++ < size / 2) {
       arr[start] ^= arr[last] ^ (arr[last--] = arr[start++]);
-      // start++;
-      // last--;
       start %= arr.length;
       last = (last + arr.length) % arr.length;
     }
--- src/lecture2/quiz2/Main.java
+++ src/lecture2/quiz2/Main.java
@@ -2,7 +2,7 @@ public class Main {
   public static void main(String[] args) {
     int[] arr = {3, 2, 5, 6, 7, 6, 5, 3, 1, 0, 0, 0};
     var arrays = new Arrays();
-    var isPal = arrays.isPalindrome(arr, 9, 2, 5);
+    var isPal = arrays.isPalindrome(arr, 9, 2, 6);
     System.out.println(isPal);
   }
 }
@@ -10,6 +10,7 @@ public class Main {
 class Arrays {
   public Boolean isPalindrome(int[] arr, int size, int start, int end) {
     if (start >= size || start < 0 || end >= size || end < 0) {
+    	System.out.println("invalid index");
       return false;
     }
     if (start > end) {
@@ -23,8 +24,4 @@ class Arrays {
     }
     return true;
   }
-
-  // private Boolean isValid(int[] arr, int size, int start, int end) {
-  //   return start < size && start >= 0 && end < size && end >= 0;
-  // }
 }
