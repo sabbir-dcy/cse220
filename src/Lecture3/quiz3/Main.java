00788f022acb3034500663be6e3c3d02f4219e35
Sabbir Ahmed <sabbirgitcodes@gmail.com>
2020-07-10 23:50:08 +0600

    c

--- src/Lecture3/quiz3/Main.java
+++ src/Lecture3/quiz3/Main.java
@@ -2,7 +2,14 @@ import java.util.Arrays;
 
 public class Main {
   public static void main(String[] args) {
-    int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 0, 0};
+    int[] arr = {0, 0, 0, 1, 2, 3, 4, 5, 6, 7, 0,0, 0, 0, 0, 0, 0, 0, 0};
+    /** 
+     1, 2, 3, 4, 5, 6, 7, 0, 0, 0, 0 // 6
+     0, 0, 0, 0, 1, 2, 3, 4, 5, 6, 7 // 6
+     3, 4, 5, 6, 7, 0, 0, 0, 0, 1, 2 // 7
+     6, 7, 0, 0, 0, 0, 1, 2, 3, 4, 5 // 7
+     0, 0, 0, 1, 2, 3, 4, 5, 6, 7, 0 // 6 or 7
+    */ 
     var modify = new Modify();
     modify.rev(arr);
     System.out.println(Arrays.toString(arr));
@@ -13,41 +20,46 @@ public class Modify {
 
   public void rev(int[] arr) {
     int len = arr.length, size = 0;
-    int i = len - 1, j = 0;
+    int i = 0;
+    int head = 0, tail = len - 1;
     
     while (true) {
-      if (i < 0) {
-        j = len - 1;
+      if (i == len) {
+        break;
+      }
+      if (arr[i] != 0) {
         i++;
+      }
+      else {
+        tail = (i + len - 1) % len;
+        while (arr[tail] == 0) {
+          tail--;
+        }
+        break;
+      }
+    }
+    while (true) {
+      if (i == len) {
         break;
       }
       if (arr[i] == 0) {
-        j = i;
-        i++;
-        break;
+        i = (i + 1) % len;
       }
       else {
-        i--;
-      }
-    }
-    while (true) {
-      if (arr[j] != 0) {
+        head = i;
         break;
-      }
-      else {
-        j--;
       }
     }
 
-    size = len - Math.abs(i - j) % (len - 1) + 1;
+    size = len - Math.abs(head - tail) % (len - 1) + 1;
     System.out.println(size);
     int c = 0;
 
     while (c++ < size / 2) {
-      // System.out.println(i +" "+ j);
-      arr[i] += arr[j] - (arr[j--] = arr[i++]);
-      i %= len;
-      j = (j + len) % len;
+      System.out.println(head +" "+ tail);
+      arr[head] += arr[tail] - (arr[tail--] = arr[head++]);
+      head %= len;
+      tail = (tail + len) % len;
     }
   }
 }
