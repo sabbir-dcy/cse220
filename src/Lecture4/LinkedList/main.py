94db8b087d5480510f453a7fa076e8faea2c2687
Sabbir Ahmed <sabbirgitcodes@gmail.com>
2020-07-14 12:20:14 +0600

    removed

---
+++ src/Lecture4/LinkedList/main.py
@@ -0,0 +1,5 @@
+def sendMail():
+  pass
+
+if (1 < 2):
+  print("greater")
---
+++ src/Lecture4/LinkedList/quiz2/Main.java
@@ -0,0 +1,35 @@
+
+public class Main {
+  public static void main(String[] args) {
+    var i = 6;
+    Node back = null;
+    while (i-- > 0) {
+      var node = new Node("one", back);
+      back = node;
+    }
+    var head = back;
+    var main = new Main();
+    String str = new String("one");
+    System.out.println(Main.contains(head, str));
+    // System.out.println(head.next.next.element);
+  }
+
+  public boolean contains(Node n, Object element) {
+    for (; n != null; n = n.next)
+      if (n.element.equals(element)) {
+        return true;
+      }
+    return false;
+  }
+}
+
+class Node {
+
+  Node next;
+  Object element;
+
+  public Node(Object element, Node next) {
+    this.element = element;
+    this.next = next;
+  }
+}
